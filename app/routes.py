9ad123b691ee3b8597b139a648182d8340974cc3
Joel Wildman <abunchofcapital@192-168-1-113.tpgi.com.au>
2022-04-29 10:02:29 +0800

    Finished template, uses css, jinja etc

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World"
+
+	user = {
+	'username': 'joj'
+	}
+
+	items = [
+		{
+			'author' : {'username': 'yourmum'},
+			'quote' : 'suffering is key'
+		},
+		{
+			'author' : {'username': 'god'},
+			'quote' : 'get fucked lmao'
+		}
+	]
+	return render_template('index.html', user=user, title="ah, sweet fervor", items=items)
 
